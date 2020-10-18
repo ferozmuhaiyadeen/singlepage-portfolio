37c17c52914a0e14924693fdd9b9f7f65f18b6b2
feroz <feroz@DESKTOP-ATPPT6R>
2020-10-18 13:54:00 +0530

    added tracker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import styled from 'styled-components';
+import { cleanData, getFingerprint } from './utils.';
 
 import About from './components/About';
 import Header from './components/Header';
@@ -20,6 +21,36 @@ const Main = styled.main`
 `;
 
 function App() {
+  const [fingerprint, setFingerprint] = useState(null);
+  const [ipData, setIpData] = useState(null);
+  const [showReport, setShowReport] = useState(true);
+
+  useEffect(() => {
+    if (showReport) {
+      fetch("https://extreme-ip-lookup.com/json")
+        .then(res => res.json())
+        .then(ip => Promise.all([ip, getFingerprint()]))
+        .then(([ip, finger]) => {
+          let f = finger
+            .map(({ key, value }) => ({ [key]: value }))
+            .reduce((acc, curr) => ({
+              ...acc,
+              ...curr
+            }));
+
+          f = cleanData(f);
+          ip = cleanData(ip);
+          console.log("hello");
+console.log(f);
+console.log(ip);
+          setFingerprint(f);
+          setIpData(ip);
+          setShowReport(false);
+        });
+    }
+  }, [showReport]);
+
+
   return (
     <Main className="App">
       <Header/>
@@ -36,6 +67,9 @@ function App() {
       <Footer/>
     </Main>
   );
+
+  
 }
+
 
 export default App;
